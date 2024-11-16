fc73c7b716772cc06b437fa1eb2c4fbbc6d8d610
ekkimukk <atvinna@yandex.ru>
2024-11-16 22:00:58 +0300

    вообще без понятия, что здесь происходило, но я начинаю делать пятую по джаве

---
+++ 5semester/frontend-app-dev/2-java-wrong-version/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Dictionary dictionary = new Dictionary();
+      dictionary.addSynonym("sing", "chant");
+      dictionary.addSynonym("sing", "intone");
+      dictionary.addSynonym("man", "male");
+      dictionary.addSynonym("man", "guy");
+      dictionary.addSynonym("man", "gent");
+      dictionary.addSynonym("hallowed", "anointed");
+      dictionary.addSynonym("hallowed", "beatified");
+
+      // String text = "sing to me of the man, muse, the man of twists and turns, driven time and again off course, once he had plundered the hallowed heights of troy";
+      String text = "sing to the man the hallowed song";
+      System.out.println(text);
+      
+      Cipher cipher = new Cipher(dictionary);
+      String encryptedText = cipher.encrypt(text);
+      System.out.println(encryptedText);
+
+      String decryptedText = cipher.decrypt(encryptedText);
+      System.out.println(decryptedText);
+    }
+}
