528509566bcea8f85e9fe6b81982ade5ef938a7b
ekkimukk <atvinna@yandex.ru>
2024-11-18 14:30:46 +0300

    третья по джаве готова окончательно

--- 5semester/frontend-app-dev/3-java/Main.java
+++ 5semester/frontend-app-dev/3-java/Main.java
@@ -3,9 +3,10 @@
 import java.util.*;
 
 class Second extends Thread {
-    public static final int N = 100_000_000;
+    public static final int N = 90_000_000;
 
     private int[] arr;
+    public int res;
 
     public Second(int[] arr) {
         this.arr = arr;
@@ -14,19 +15,21 @@ class Second extends Thread {
 	@Override public void run() {
 		{
             int mean = 0;
-            for (int i = 0; i < N; i++) {
+            for (int i = 50_000_000; i < 70_000_000; i++) {
                 mean += arr[i];
             }
             mean /= N;
             System.out.println("2 > " + mean);
+            this.res = mean;
         }
     }
 }
 
 class Third extends Thread {
     private int[] arr;
+    public int res;
 
-    public static final int N = 100_000_000;
+    public static final int N = 90_000_000;
 
     public Third(int[] arr) {
         this.arr = arr;
@@ -35,17 +38,18 @@ class Third extends Thread {
 	@Override public void run() {
 		{
             int mean = 0;
-            for (int i = 0; i < N; i++) {
+            for (int i = 70_000_000; i < 90_000_000; i++) {
                 mean += arr[i];
             }
             mean /= N;
             System.out.println("3 > " + mean);
+            this.res = mean;
         }
     }
 }
 
 class Main {
-    public static final int N = 100_000_000;
+    public static final int N = 90_000_000;
 
 	static Second second;
 	static Third third;
@@ -63,14 +67,16 @@ class Main {
 		second.start();
         third.start();
 
+        int mean = 0;
         {
-            int mean = 0;
-            for (int i = 0; i < N; i++) {
+            for (int i = 0; i < 50_000_000; i++) {
                 mean += arr[i];
             }
             mean /= N;
             System.out.println("1 > " + mean);
         }
+
+        System.out.println((mean + second.res + third.res) / 3);
 
 		// if (second.isAlive() && third.isAlive()) {
 		// 	try {
