dfd88d42ed805bc9eba23ca96bd2c653397aa261
ekkimukk <atvinna@yandex.ru>
2024-11-18 15:14:17 +0300

    третья по джаве точно готова

--- 5semester/frontend-app-dev/4-java/Document.java
+++ 5semester/frontend-app-dev/4-java/Document.java
@@ -1,34 +1,63 @@
 import java.util.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Document {
-    private String content = "#";
+    private String content;
 
     public Document(String data) {
         this.content = data;
     }
 
     public String create() {
-        return "Creating document " + content;
+        System.out.println("Creating document \"" + content + "\"");
+        try {
+            File f = new File(content);
+            if (f.createNewFile()) {
+                return "File created: " + f.getName();
+            } else {
+                return "File already exists";
+            }
+        } catch (Exception e) {
+            return "An error occurred.";
+        }
     }
 
     public String undoCreate() {
-        return "Undo creating document " + content;
+        System.out.println("Undo creating document \"" + content + "\"");
+        return "";
     }
 
     public String edit() {
-        return "Editing document " + content;
+        System.out.println("Editing document \"" + content + "\"");
+        try {
+            FileWriter myWriter = new FileWriter(content);
+            myWriter.write("ICH LIEBE CAPIBARA!!");
+            myWriter.close();
+            return "Successfully edit the file.";
+        } catch (IOException e) {
+            return "An error occurred.";
+        }
     }
 
     public String undoEdit() {
-        return "Undo editing document " + content;
+        System.out.println("Undo editing document \"" + content + "\"");
+        return "";
     }
 
     public String delete() {
-        return "Deleting document " + content;
+        System.out.println("Deleting document \"" + content + "\"");
+        File myObj = new File(content);
+        if (myObj.delete()) {
+            return "Deleted the file: \"" + myObj.getName() + "\"";
+        } else {
+            return "Failed to delete the file";
+        }
     }
 
     public String undoDelete() {
-        return "Undo deleting document " + content;
+        return "Undo deleting document \"" + content + "\"";
     }
 }
 
